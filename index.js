28b3a7810d3041bd1dd46d41bc2e87545c68f62b
Mykola Kolisnyk <kolesnikny@gmail.com>
2022-01-11 13:10:00 +0200

    Add 5 task

--- index.js
+++ index.js
@@ -3,17 +3,41 @@ const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 
 const arrayConcat = array1.concat(array2);
-console.log(arrayConcat);
+//console.log(arrayConcat);
 
 /*2. Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1](reverse).*/
+const arrayReverse = array1.slice();
+
+arrayReverse.reverse();
+//console.log(arrayReverse);
 
 /*3. Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6*/
 
+const arrayPush = array1.slice();
+
+arrayPush.push(4, 5, 6);
+//console.log(arrayPush);
+
 /*4. Дан массив [1, 2, 3]. Добавьте ему в начало 7, 8, 9.*/
+
+const arrayUnshift = array1.slice();
+
+arrayUnshift.unshift(7, 8, 9);
+//console.log(arrayUnshift);
 
 /*5. Дан массив ['aaa', 'bbb', 'ccc']. Выведите на экран первый элемент и удалите его.*/
 
+const arrayShift = ['aaa', 'bbb', 'ccc'];
+
+console.log(arrayShift.shift());
+// console.log(arrayShift);
+
 /*6. Дан массив ['ddd', 'fff', 'eee']. Выведите последний элемент на экран.*/
+
+const arrayPop = ['aaa', 'bbb', 'ccc'];
+
+//console.log(arrayPop.pop());
+//console.log(arrayPop);
 
 /*7. Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый массив элементы с третьего по пятый.*/
 
